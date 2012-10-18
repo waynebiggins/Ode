87f2c71b2f16bc5ea909f6946e5e1f4b6cd52379
Neil Potter <n.potter@ljmu.ac.uk>
2012-10-18 15:30:15 +0100

    Initial Commit

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            var model = new RestaurantReview()
+            {
+                Name = "Jim Baxters",
+                Rating = 5
+            };
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
