92e1bc8b00316b58cd6e52c9ee46ede090a416a6
Neil Potter <n.potter@ljmu.ac.uk>
2012-10-31 18:09:44 +0000

    Changed Home Controler

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using OdeToFood.Models;
+using OdeToFood.Queries;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
-       
+        //FoodDb _db = new FoodDb();
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            //ViewBag.Message = "Welcome to ASP.NET MVC!";
+            //var model = _db.Reviews.FindTheBest();
             //var model = new Review();
             //model.Name = "Jim Baxter";
             //model.Rating = 5;
