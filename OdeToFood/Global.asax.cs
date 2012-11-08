19f37c17931bd8471fc34627d107ad9654c9c5c2
Neil Potter <n.potter@ljmu.ac.uk>
2012-11-08 18:50:27 +0000

    Changed global asax to seed data

--- OdeToFood/Global.asax.cs
+++ OdeToFood/Global.asax.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Data.Entity;
+using OdeToFood.Models;
+
 
 namespace OdeToFood
 {
@@ -37,10 +40,56 @@ namespace OdeToFood
 
         protected void Application_Start()
         {
+           // Database.SetInitializer(new OdeToFoodDBInitializer());
+
             AreaRegistration.RegisterAllAreas();
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
+        }
+    }
+
+    public class OdeToFoodDBInitializer : DropCreateDatabaseIfModelChanges<OdeToFoodDB>
+    {
+        protected override void Seed(OdeToFoodDB context)
+        {
+            base.Seed(context);
+
+            context.Restaurants.Add(new Restaurant
+            {
+                Name = "Marrakesh",
+                ChefsName="Jeff",
+                Address = new Address
+                
+                {
+                    City = "Washington",
+                    State = "D.C.",
+                    Country = "USA"
+                }
+            });
+
+            context.Restaurants.Add(new Restaurant
+           {
+               Name = "Sabationo's",
+               Address = new Address
+               {
+                   City = "Baltimore",
+                   State = "MD",
+                   Country = "USA"
+               }
+           });
+
+            context.Restaurants.Add(new Restaurant
+            {
+                Name = "The Kings",
+                Address = new Address
+                {
+                    City = "Columbia",
+                    State = "MD",
+                    Country = "USA"
+                }
+            });
+            context.SaveChanges();
         }
     }
 }
